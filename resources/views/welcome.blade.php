ec075a211251974d933f6bad1664cabddaf26ad5
benny <bennyvg89@gmail.com>
2019-07-18 20:14:30 +0530

    test1

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Benny George
                 </div>
 
                 <div class="links">
